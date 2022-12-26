7c13e89621463099c8e538d747f74f36972a678e
AnilBamne <anilbamne20@gmail.com>
2022-12-26 23:39:17 +0530

    Adding:UC1CreateAddressBookDataBase code

---
+++ AddressBookUsingADO.Net/AddressBookBuilder.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookUsingADO.Net
+{
+    public class AddressBookBuilder
+    {
+        public static string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=AddressBookService;Integrated Security=True;";
+        SqlConnection connection = new SqlConnection(connectionString);
+
+        /// <summary>
+        /// Establishing connection with database
+        /// </summary>
+        public void CheckConnection()
+        {
+            try
+            {
+                this.connection.Open();
+                Console.WriteLine("Connection established succesfully");
+                this.connection.Close();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                this.connection.Close();
+            }
+        }
+    }
+}
--- AddressBookUsingADO.Net/Program.cs
+++ AddressBookUsingADO.Net/Program.cs
@@ -7,6 +7,30 @@ namespace AddressBookUsingADO.Net
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Address book system - Using ADO.Net ");
+            while (true)
+            {
+                AddressBookBuilder builder = new AddressBookBuilder();
+                Console.WriteLine("enter option\n1 Check connection\n2 Insert new contact\n0  exit");
+                int option=int.Parse(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        //uc1 create addressbook DB
+                        builder.CheckConnection();
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    case 5:
+                        break;
+                    case 0:
+                        Environment.Exit(0);
+                        break;
+                }
+            }
         }
     }
 }
