d109b9187a7ea21ba66e54c0c2c2a72cdcf1ee64
AnilBamne <anilbamne20@gmail.com>
2022-12-26 23:22:11 +0530

    Adding:Welcome message

---
+++ AddressBookUsingADO.Net/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AddressBookUsingADO.Net
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Address book system - Using ADO.Net ");
+        }
+    }
+}
